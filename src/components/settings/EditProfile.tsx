286b1b81d08c03e856fbc583701e406814122c4b
0xm <tan100995@proton.me>
2025-03-30 01:42:52 -0500

    Update placeholder

--- src/components/settings/EditProfile.tsx
+++ src/components/settings/EditProfile.tsx
@@ -237,7 +237,7 @@ const EditProfile: React.FC = () => {
               type="password"
               name="password"
               value={formData.password}
-              placeholder='11111111'
+              placeholder='********'
               onChange={handleInputChange}
               className="w-full px-3 py-2 border border-input-border rounded-xl placeholder-input-placeholder focus:outline-none focus:ring-2 focus:ring-input-focus focus:border-transparent"
             />
